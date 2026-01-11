b724445f1739d08d2cc0e9109c7435b618f805ff
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-11 22:29:24 +0400

    Add histogram of student grades

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+4-frequency.py
+Plots a histogram showing the distribution of student grades.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Displays a histogram of student scores for Project A.
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+
+    plt.show()
