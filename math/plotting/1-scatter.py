ad22bb67f8d8d60b270de26810c4b0424c46f2c5
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-11 22:24:42 +0400

    Add scatter plot

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+1-scatter.py
+Plots a scatter graph of men's height versus weight.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    Generates a scatter plot with height on the x-axis
+    and weight on the y-axis using magenta points.
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.scatter(x, y, c='m')
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.title("Men's Height vs Weight")
+
+    plt.show()
