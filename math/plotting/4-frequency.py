75fb331c007f081af0e7064809ff3db3ea928663
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-11 22:36:52 +0400

    Match histogram reference exactly

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
+"""histogram of student scores"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def frequency():
-    """Plots a histogram of student scores for a project"""
+    """
+    The x-axis should be labeled Grades
+    The y-axis should be labeled Number of Students
+    The x-axis should have bins every 10 units
+    The title should be Project A
+    The bars should be outlined in black
+    """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
-    # Define bins every 10 units from 0 to 100
-    bins = range(0, 101, 10)
-
-    # Plot the histogram
-    # edgecolor='black' outlines the bars
+    bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
     plt.hist(student_grades, bins=bins, edgecolor='black')
-
-    # Set the labels and title
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
-    plt.title('Project A')
-
-    # Set x-axis ticks to match the bins for clarity
     plt.xticks(bins)
-
-    plt.show()
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
