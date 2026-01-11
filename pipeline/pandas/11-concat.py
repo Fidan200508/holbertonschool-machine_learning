a7ae41012eed00c68a77d0753010522371400afd
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-11 22:01:09 +0400

    Concatenate bitstamp and coinbase data with keys

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Concatenates two DataFrames indexed by Timestamp with source labels.
+"""
+
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """
+    Indexes both DataFrames on Timestamp, selects rows from df2 up to a
+    given timestamp, and concatenates df2 above df1 with source keys.
+
+    Args:
+        df1 (pd.DataFrame): Coinbase DataFrame.
+        df2 (pd.DataFrame): Bitstamp DataFrame.
+
+    Returns:
+        pd.DataFrame: Concatenated DataFrame with labeled keys.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2 = df2[df2.index <= 1417411920]
+
+    return pd.concat(
+        [df2, df1],
+        keys=["bitstamp", "coinbase"]
+    )
