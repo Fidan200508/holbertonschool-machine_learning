aac640851f67059eed6316988c27a76183d040e7
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-02-01 19:35:58 +0000

    Fix shebang and documentation for np_shape

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,2 +1,7 @@
+#!/usr/bin/env python3
+"""Module that contains function to get shape of a numpy.ndarray"""
+
+
 def np_shape(matrix):
+    """Returns the shape of a numpy.ndarray as a tuple"""
     return matrix.shape
