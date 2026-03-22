b8267a0043b85916b0a67c0f35d7e848674d275c
Fidan <allahverdiyeva200508@gmail.com>
2026-03-22 19:36:10 +0400

    Add l2_reg_create_layer function

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python3
-"""Create a TensorFlow layer with L2 regularization"""
+"""
+Contains the function l2_reg_create_layer
+"""
 import tensorflow as tf
+
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Creates a layer in TensorFlow with L2 regularization
-
-    prev: tensor, output of the previous layer
-    n: number of nodes in the new layer
-    activation: activation function to use
-    lambtha: L2 regularization parameter
-
-    Returns: output tensor of the new layer
+    Creates a neural network layer in TensorFlow that includes L2 regularization
+    Args:
+        prev: tensor containing the output of the previous layer
+        n: number of nodes the new layer should contain
+        activation: activation function that should be used on the layer
+        lambtha: L2 regularization parameter
+    Returns: the output of the new layer
     """
+    # Define the weight initializer (He et al. / Variance Scaling)
+    init = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg',
+        distribution='uniform'
+    )
+
+    # Define the L2 regularizer
+    reg = tf.keras.regularizers.L2(l2=lambtha)
+
+    # Create the Dense layer
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_regularizer=tf.keras.regularizers.L2(l2=lambtha)
-    )(prev)
-    return layer
+        kernel_initializer=init,
+        kernel_regularizer=reg
+    )
+
+    return layer(prev)
