884383a70ad0cd6b594c9138039eb606f5324a17
Fidan <allahverdiyeva200508@gmail.com>
2026-03-22 20:22:06 +0400

    Add function to create layer with L2 regularization in TensorFlow

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,6 +1,7 @@
-
- #!/usr/bin/env python3
-"""Module to create a layer with L2 regularization"""
+#!/usr/bin/env python3
+"""
+Module to create a TensorFlow layer with L2 regularization
+"""
 import tensorflow as tf
 
 
@@ -11,27 +12,30 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Args:
         prev: tensor containing the output of the previous layer
         n: number of nodes the new layer should contain
-        activation: activation function for the new layer
+        activation: activation function that should be used on the layer
         lambtha: L2 regularization parameter
 
     Returns:
-        the output of the new layer
+        The output of the new layer
+    }
     """
-    # He et al. initialization is standard for layers with activations
+    # Initialize the L2 regularizer with the given lambtha
+    regularizer = tf.keras.regularizers.L2(lambtha)
+
+    # Initialize weights using He Normal (standard for deep networks)
+    # for consistency with most Holberton/ALX projects
     initializer = tf.keras.initializers.VarianceScaling(
         scale=2.0,
-        mode='fan_avg'
+        mode='fan_avg',
+        distribution='uniform'
     )
 
-    # Define the L2 regularizer
-    regularizer = tf.keras.regularizers.L2(l2=lambtha)
-
-    # Create the Dense layer with kernel_regularizer
+    # Create the Dense layer
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_initializer=initializer,
-        kernel_regularizer=regularizer
+        kernel_regularizer=regularizer,
+        kernel_initializer=initializer
     )
 
     return layer(prev)
