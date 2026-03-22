d9c31da856e30759518b53051d2d46a197b8eeae
Fidan <allahverdiyeva200508@gmail.com>
2026-03-22 19:20:17 +0400

    Add l2_reg_gradient_descent function for gradient descent with L2 regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Gradient descent with L2 regularization"""
-
 import numpy as np
 
 
@@ -9,32 +8,29 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     Updates the weights and biases of a neural network using
     gradient descent with L2 regularization.
 
-    Parameters:
-    Y: one-hot numpy.ndarray of shape (classes, m) with correct labels
+    Y: one-hot numpy.ndarray of shape (classes, m)
     weights: dictionary of weights and biases
-    cache: dictionary of layer outputs
+    cache: dictionary of outputs of each layer
     alpha: learning rate
     lambtha: L2 regularization parameter
-    L: number of layers
+    L: number of layers in the network
     """
-
     m = Y.shape[1]
+    dZ = cache[f"A{L}"] - Y
 
-    # Initialize dZ as difference for last layer
-    dZ = cache["A{}".format(L)] - Y
+    for l in reversed(range(1, L + 1)):
+        A_prev = cache[f"A{l-1}"]
+        W = weights[f"W{l}"]
 
-    for i in reversed(range(1, L + 1)):
-        A_prev = cache["A{}".format(i - 1)]
-        W = weights["W{}".format(i)]
-
-        # Compute gradients
+        # Gradient of weights with L2 regularization
         dW = (np.matmul(dZ, A_prev.T) / m) + (lambtha / m) * W
         db = np.sum(dZ, axis=1, keepdims=True) / m
 
         # Update weights and biases
-        weights["W{}".format(i)] -= alpha * dW
-        weights["b{}".format(i)] -= alpha * db
+        weights[f"W{l}"] -= alpha * dW
+        weights[f"b{l}"] -= alpha * db
 
-        if i > 1:
-            # Backpropagate dZ to previous layer using tanh derivative
-            dZ = np.matmul(W.T, dZ) * (1 - np.power(A_prev, 2))
+        # Compute dZ for previous layer if not input
+        if l > 1:
+            dA_prev = np.matmul(W.T, dZ)
+            dZ = dA_prev * (1 - np.power(A_prev, 2))  # tanh derivative
