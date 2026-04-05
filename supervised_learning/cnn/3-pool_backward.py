8bd50d98613fb2dba9ec1c911b7a0076cb3f9f22
Fidan <allahverdiyeva200508@gmail.com>
2026-04-06 00:10:40 +0400

    Implement pool_backward for max and average pooling

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -3,48 +3,34 @@ import numpy as np
 
 def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """
-    Performs back propagation over a pooling layer.
-
-    dA: np.ndarray of shape (m, h_new, w_new, c_new)
-        Partial derivatives w.r.t the output of the pooling layer
-    A_prev: np.ndarray of shape (m, h_prev, w_prev, c)
-        Output of the previous layer
-    kernel_shape: tuple (kh, kw)
-        Size of the pooling kernel
-    stride: tuple (sh, sw)
-        Stride for pooling
-    mode: 'max' or 'avg'
-        Pooling mode
-
-    Returns:
-        dA_prev: np.ndarray of shape (m, h_prev, w_prev, c)
-        Partial derivatives w.r.t the previous layer
+    Performs back propagation over a pooling layer of a neural network
     """
-    m, h_new, w_new, c = dA.shape
-    _, h_prev, w_prev, _ = A_prev.shape
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    m, h_new, w_new, c_new = dA.shape
     kh, kw = kernel_shape
     sh, sw = stride
 
     # Initialize dA_prev with zeros
     dA_prev = np.zeros_like(A_prev)
 
-    for i in range(m):
+    for i in range(m):  # loop over examples
         for h in range(h_new):
             for w in range(w_new):
-                for ch in range(c):
-                    h_start = h * sh
-                    h_end = h_start + kh
-                    w_start = w * sw
-                    w_end = w_start + kw
+                for c in range(c_new):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
 
                     if mode == 'max':
-                        # Create mask of max value
-                        A_slice = A_prev[i, h_start:h_end, w_start:w_end, ch]
-                        mask = (A_slice == np.max(A_slice))
-                        dA_prev[i, h_start:h_end, w_start:w_end, ch] += mask * dA[i, h, w, ch]
+                        # slice from A_prev
+                        a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+                        mask = (a_prev_slice == np.max(a_prev_slice))
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += mask * dA[i, h, w, c]
                     elif mode == 'avg':
-                        # Distribute gradient evenly
-                        da = dA[i, h, w, ch] / (kh * kw)
-                        dA_prev[i, h_start:h_end, w_start:w_end, ch] += np.ones((kh, kw)) * da
+                        da = dA[i, h, w, c]
+                        shape = (kh, kw)
+                        average = da / (kh * kw)
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += np.ones(shape) * average
 
     return dA_prev
