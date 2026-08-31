d615b228c2188159c2b2bc12fb2078eea95cd5a8
Fidan <allahverdiyeva200508@gmail.com>
2026-08-31 23:55:48 +0400

    Fix FastText model

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""FastText training module."""
+"""FastText model"""
 
 import gensim
 
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                    window=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Create, build, and train a Gensim FastText model."""
+    """Creates and trains a FastText model."""
     model = gensim.models.FastText(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
-        negative=negative,
         window=window,
-        sg=not cbow,
+        negative=negative,
+        sg=0 if cbow else 1,
         epochs=epochs,
         seed=seed,
         workers=workers
