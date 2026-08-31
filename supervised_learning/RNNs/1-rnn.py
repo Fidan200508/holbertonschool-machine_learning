ee012d390c99f2846b2fff6221d03786a7e6cecf
Fidan <allahverdiyeva200508@gmail.com>
2026-08-31 21:18:32 +0400

    Implement RNN forward propagation

---
+++ supervised_learning/RNNs/1-rnn.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Forward propagation for a simple RNN."""
+
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """Perform forward propagation for a simple RNN.
+
+    Args:
+        rnn_cell: RNNCell instance used for forward propagation.
+        X: Input data of shape (t, m, i).
+        h_0: Initial hidden state of shape (m, h).
+
+    Returns:
+        H: All hidden states.
+        Y: All outputs.
+    """
+    t, m, _ = X.shape
+    h = h_0.shape[1]
+    o = rnn_cell.Wy.shape[1]
+
+    H = np.zeros((t + 1, m, h))
+    Y = np.zeros((t, m, o))
+
+    H[0] = h_0
+
+    for step in range(t):
+        h_next, y = rnn_cell.forward(
+            H[step],
+            X[step]
+        )
+
+        H[step + 1] = h_next
+        Y[step] = y
+
+    return H, Y
