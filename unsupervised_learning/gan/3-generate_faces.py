ab46d7494a61b28de8a352f74705ddf45b2f09a1
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 00:34:39 +0400

    Implement convolutional GAN for face generation

---
+++ unsupervised_learning/gan/3-generate_faces.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+"""Build convolutional generator and discriminator models."""
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+def convolutional_GenDiscr():
+    """Create convolutional generator and discriminator models."""
+
+    def get_generator():
+        """Build the generator model."""
+        inputs = keras.Input(shape=(16,))
+
+        x = keras.layers.Dense(
+            2048,
+            activation="tanh"
+        )(inputs)
+
+        x = keras.layers.Reshape(
+            (2, 2, 512)
+        )(x)
+
+        x = keras.layers.UpSampling2D()(x)
+
+        x = keras.layers.Conv2D(
+            64,
+            kernel_size=(3, 3),
+            padding="same"
+        )(x)
+
+        x = keras.layers.BatchNormalization()(x)
+        x = keras.layers.Activation("tanh")(x)
+
+        x = keras.layers.UpSampling2D()(x)
+
+        x = keras.layers.Conv2D(
+            16,
+            kernel_size=(3, 3),
+            padding="same"
+        )(x)
+
+        x = keras.layers.BatchNormalization()(x)
+        x = keras.layers.Activation("tanh")(x)
+
+        x = keras.layers.UpSampling2D()(x)
+
+        x = keras.layers.Conv2D(
+            1,
+            kernel_size=(3, 3),
+            padding="same"
+        )(x)
+
+        x = keras.layers.BatchNormalization()(x)
+        outputs = keras.layers.Activation("tanh")(x)
+
+        return keras.Model(
+            inputs,
+            outputs,
+            name="generator"
+        )
+
+    def get_discriminator():
+        """Build the discriminator model."""
+        inputs = keras.Input(
+            shape=(16, 16, 1)
+        )
+
+        x = keras.layers.Conv2D(
+            32,
+            kernel_size=(3, 3),
+            padding="same"
+        )(inputs)
+
+        x = keras.layers.MaxPooling2D()(x)
+        x = keras.layers.Activation("tanh")(x)
+
+        x = keras.layers.Conv2D(
+            64,
+            kernel_size=(3, 3),
+            padding="same"
+        )(x)
+
+        x = keras.layers.MaxPooling2D()(x)
+        x = keras.layers.Activation("tanh")(x)
+
+        x = keras.layers.Conv2D(
+            128,
+            kernel_size=(3, 3),
+            padding="same"
+        )(x)
+
+        x = keras.layers.MaxPooling2D()(x)
+        x = keras.layers.Activation("tanh")(x)
+
+        x = keras.layers.Conv2D(
+            256,
+            kernel_size=(3, 3),
+            padding="same"
+        )(x)
+
+        x = keras.layers.MaxPooling2D()(x)
+        x = keras.layers.Activation("tanh")(x)
+
+        x = keras.layers.Flatten()(x)
+
+        outputs = keras.layers.Dense(
+            1,
+            activation="tanh"
+        )(x)
+
+        return keras.Model(
+            inputs,
+            outputs,
+            name="discriminator"
+        )
+
+    return get_generator(), get_discriminator()
