f401be72c3cbff8ca7459eb09549b96e65cdb189
Fidan <allahverdiyeva200508@gmail.com>
2026-08-31 22:14:51 +0400

    Fix Word2Vec vocabulary ordering

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -4,26 +4,18 @@
 import gensim
 
 
-def word2vec_model(
-    sentences,
-    vector_size=100,
-    min_count=5,
-    window=5,
-    negative=5,
-    cbow=True,
-    epochs=5,
-    seed=0,
-    workers=1
-):
+def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
+                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Create, build, and train a Word2Vec model."""
     model = gensim.models.Word2Vec(
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=0 if cbow else 1,
+        sg=not cbow,
         seed=seed,
-        workers=workers
+        workers=workers,
+        sorted_vocab=0
     )
 
     model.build_vocab(sentences)
@@ -33,5 +25,7 @@ def word2vec_model(
         total_examples=model.corpus_count,
         epochs=epochs
     )
+
+    model.wv.sort_by_descending_frequency()
 
     return model
