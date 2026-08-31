d024708ed6474d38588853d44b4eb3fba320efd6
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 01:11:39 +0400

    Add Normal PDF

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -53,3 +53,26 @@ class Normal:
             z * self.stddev
             + self.mean
         )
+
+    def pdf(self, x):
+        """Calculate the PDF for a given x-value."""
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        coefficient = (
+            1
+            / (
+                self.stddev
+                * (2 * pi) ** 0.5
+            )
+        )
+
+        exponent = (
+            -0.5
+            * (
+                (x - self.mean)
+                / self.stddev
+            ) ** 2
+        )
+
+        return coefficient * (e ** exponent)
