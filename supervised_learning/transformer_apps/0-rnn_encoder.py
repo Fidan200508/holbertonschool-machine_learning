a7a4a5c25ecb5df5ac4e336aeacb3b0f3b793a8a
Fidan <allahverdiyeva200508@gmail.com>
2026-08-31 20:31:40 +0400

    Implement RNN encoder

---
+++ supervised_learning/transformer_apps/0-rnn_encoder.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""RNN encoder module for machine translation."""
+
+import tensorflow as tf
+
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """RNN encoder using an embedding layer and GRU."""
+
+    def __init__(self, vocab, embedding, units, batch):
+        """Initialize the RNN encoder.
+
+        Args:
+            vocab: Size of the input vocabulary.
+            embedding: Dimensionality of embedding vectors.
+            units: Number of hidden units in the GRU.
+            batch: Batch size.
+        """
+        super().__init__()
+
+        self.batch = batch
+        self.units = units
+
+        self.embedding = tf.keras.layers.Embedding(
+            vocab,
+            embedding
+        )
+
+        self.gru = tf.keras.layers.GRU(
+            units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform'
+        )
+
+    def initialize_hidden_state(self):
+        """Initialize the GRU hidden state with zeros.
+
+        Returns:
+            Tensor of shape (batch, units).
+        """
+        return tf.zeros(
+            (self.batch, self.units)
+        )
+
+    def call(self, x, initial):
+        """Perform the forward pass of the encoder.
+
+        Args:
+            x: Tensor of shape (batch, input_seq_len).
+            initial: Initial hidden state of shape (batch, units).
+
+        Returns:
+            outputs: GRU outputs for all time steps.
+            hidden: Final hidden state.
+        """
+        x = self.embedding(x)
+
+        outputs, hidden = self.gru(
+            x,
+            initial_state=initial
+        )
+
+        return outputs, hidden
