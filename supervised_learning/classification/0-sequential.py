34ea20de82ff4355c473eaa4d60d6e551baffa3f
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 11:51:56 +0400

    Build Keras sequential model

---
+++ supervised_learning/classification/0-sequential.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Builds a neural network using Keras Sequential."""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """Build a neural network using the Keras Sequential API."""
+    model = K.Sequential()
+
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(
+                K.layers.Dense(
+                    layers[i],
+                    activation=activations[i],
+                    kernel_regularizer=K.regularizers.l2(lambtha),
+                    input_shape=(nx,)
+                )
+            )
+        else:
+            model.add(
+                K.layers.Dense(
+                    layers[i],
+                    activation=activations[i],
+                    kernel_regularizer=K.regularizers.l2(lambtha)
+                )
+            )
+
+        if i < len(layers) - 1:
+            model.add(
+                K.layers.Dropout(
+                    1 - keep_prob
+                )
+            )
+
+    return model
