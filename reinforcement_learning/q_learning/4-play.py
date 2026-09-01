da8fe814c7b567a440be65f70674225ca21867b2
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 14:28:15 +0400

    Add Q-learning play function

---
+++ reinforcement_learning/q_learning/4-play.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Allows a trained Q-learning agent to play an episode."""
+
+import numpy as np
+
+
+def play(env, Q, max_steps=100):
+    """Play an episode by always exploiting the Q-table."""
+    state = env.reset()[0]
+    total_rewards = 0
+    rendered_outputs = [env.render()]
+
+    for _ in range(max_steps):
+        action = np.argmax(Q[state])
+
+        state, reward, terminated, truncated, _ = env.step(action)
+
+        total_rewards += reward
+        rendered_outputs.append(env.render())
+
+        if terminated or truncated:
+            break
+
+    return total_rewards, rendered_outputs
