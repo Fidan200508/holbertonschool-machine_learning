49e39d1ba24925c4777ff779ccd1d3d45f1e3f3c
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 12:11:30 +0400

    Add one hot encoding

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Converts labels into a one-hot matrix."""
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """Convert a label vector into a one-hot matrix."""
+    return K.utils.to_categorical(
+        labels,
+        num_classes=classes
+    )
