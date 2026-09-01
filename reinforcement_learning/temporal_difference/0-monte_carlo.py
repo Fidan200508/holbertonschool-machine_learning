3e9f720d0e2147a51380600293cf45d6533e067b
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 14:38:06 +0400

    Add Monte Carlo value estimation

---
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Performs Monte Carlo value estimation."""
+
+
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
+                alpha=0.1, gamma=0.99):
+    """Update state-value estimates using the Monte Carlo algorithm."""
+    for _ in range(episodes):
+        state = env.reset()[0]
+        episode = []
+
+        for _ in range(max_steps):
+            action = policy(state)
+
+            next_state, reward, terminated, truncated, _ = env.step(action)
+
+            episode.append((state, reward))
+            state = next_state
+
+            if terminated or truncated:
+                break
+
+        G = 0
+
+        for i in range(len(episode) - 1, -1, -1):
+            state, reward = episode[i]
+            G = reward + gamma * G
+
+            previous_states = [step[0] for step in episode[:i]]
+
+            if state not in previous_states:
+                V[state] = V[state] + alpha * (
+                    G - V[state]
+                )
+
+    return V
