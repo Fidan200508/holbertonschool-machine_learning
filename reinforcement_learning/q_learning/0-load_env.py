ece7e04bd90ba7caf865914b8a0bb681046f3b3a
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 14:20:35 +0400

    Fix FrozenLake environment loader

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -6,9 +6,6 @@ import gymnasium as gym
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """Load and return a FrozenLake environment."""
-    if desc is None and map_name is None:
-        desc = gym.envs.toy_text.frozen_lake.generate_random_map(size=8)
-
     env = gym.make(
         'FrozenLake-v1',
         desc=desc,
