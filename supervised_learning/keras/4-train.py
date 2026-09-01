d72971546a90263d7c76cefd64a9a3cc6fc65678
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 12:15:12 +0400

    Add Keras model training

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Trains a Keras model."""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """Train a model using mini-batch gradient descent."""
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+
+    return history
