8b3429385c6fe7128958c7472d64148d6bc322af
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 13:45:59 +0400

    Add TensorFlow learning rate decay

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Creates an inverse time learning rate decay schedule."""
+
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """Create a stepwise inverse time learning rate decay schedule."""
+    return tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True
+    )
