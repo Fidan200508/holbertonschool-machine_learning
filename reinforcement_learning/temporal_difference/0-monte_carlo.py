18cd1b2bbbc39c2618b0508e925482d180f41e69
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 14:47:59 +0400

    Fix Monte Carlo numpy import

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Performs Monte Carlo value estimation."""
 
+import numpy as np
+
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
@@ -11,7 +13,6 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
 
         for _ in range(max_steps):
             action = policy(state)
-
             next_state, reward, terminated, truncated, _ = env.step(action)
 
             episode.append((state, reward))
@@ -26,11 +27,11 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
             state, reward = episode[i]
             G = reward + gamma * G
 
-            previous_states = [step[0] for step in episode[:i]]
+            previous_states = [
+                step[0] for step in episode[:i]
+            ]
 
             if state not in previous_states:
-                V[state] = V[state] + alpha * (
-                    G - V[state]
-                )
+                V[state] += alpha * (G - V[state])
 
     return V
