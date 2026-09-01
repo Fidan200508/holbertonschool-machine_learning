a3cbba71bfb3b9a920f37c637bf457cfe576ef1d
Fidan <allahverdiyeva200508@gmail.com>
2026-09-01 13:02:02 +0400

    Fix Keras test model import

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Tests a Keras neural network model."""
 
+import tensorflow.keras as K
+
 
 def test_model(network, data, labels, verbose=True):
     """Test a neural network and return its loss and accuracy."""
