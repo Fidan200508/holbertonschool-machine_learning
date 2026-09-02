ed4b1c88d888528bfd206c0420ec1b0d2d56e76e
Fidan <allahverdiyeva200508@gmail.com>
2026-09-02 16:19:26 +0400

    Fix Monte Carlo every visit update

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-"""Performs Monte Carlo value estimation."""
+"""Performs the Monte Carlo algorithm."""
 
 import numpy as np
 
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
-    """Update state-value estimates using the Monte Carlo algorithm."""
+    """Perform Monte Carlo value estimation."""
     for _ in range(episodes):
         state = env.reset()[0]
         episode = []
 
         for _ in range(max_steps):
             action = policy(state)
+
             next_state, reward, terminated, truncated, _ = env.step(action)
 
             episode.append((state, reward))
@@ -23,15 +24,8 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
 
         G = 0
 
-        for i in range(len(episode) - 1, -1, -1):
-            state, reward = episode[i]
+        for state, reward in reversed(episode):
             G = reward + gamma * G
-
-            previous_states = [
-                step[0] for step in episode[:i]
-            ]
-
-            if state not in previous_states:
-                V[state] += alpha * (G - V[state])
+            V[state] += alpha * (G - V[state])
 
     return V
