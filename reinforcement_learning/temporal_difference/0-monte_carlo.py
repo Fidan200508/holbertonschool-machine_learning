7f0538fa98ad01eb7caddf4a985f7ab6617ba752
Fidan <allahverdiyeva200508@gmail.com>
2026-09-02 17:18:21 +0400

    Fix first visit Monte Carlo

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
-    """Perform Monte Carlo value estimation."""
+    """Perform first-visit Monte Carlo value estimation."""
     for _ in range(episodes):
         state = env.reset()[0]
         episode = []
@@ -17,6 +17,7 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
             next_state, reward, terminated, truncated, _ = env.step(action)
 
             episode.append((state, reward))
+
             state = next_state
 
             if terminated or truncated:
@@ -24,8 +25,11 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
 
         G = 0
 
-        for state, reward in reversed(episode):
+        for i in range(len(episode) - 1, -1, -1):
+            state, reward = episode[i]
             G = reward + gamma * G
-            V[state] += alpha * (G - V[state])
+
+            if state not in [x[0] for x in episode[:i]]:
+                V[state] += alpha * (G - V[state])
 
     return V
