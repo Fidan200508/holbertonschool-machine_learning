2963caa4444a6516eb7af2ed8f3ab4bc07986476
Fidan <allahverdiyeva200508@gmail.com>
2026-09-02 18:33:45 +0400

    Add policy gradient training animation

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -1,47 +1,54 @@
 #!/usr/bin/env python3
-"""Training function for Monte Carlo policy gradient."""
+"""Training using Monte Carlo policy gradient."""
 
 import numpy as np
 
 policy_gradient = __import__('policy_gradient').policy_gradient
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98,
+          show_result=False):
     """Train an agent using the Monte Carlo policy gradient algorithm."""
     weight = np.random.rand(
         env.observation_space.shape[0],
         env.action_space.n
     )
+
     scores = []
 
     for episode in range(nb_episodes):
         state, _ = env.reset()
+
         gradients = []
         rewards = []
-        done = False
+        score = 0
 
-        while not done:
+        terminated = False
+        truncated = False
+
+        while not (terminated or truncated):
+            if show_result and episode % 1000 == 0:
+                env.render()
+
             action, gradient = policy_gradient(state, weight)
 
-            next_state, reward, terminated, truncated, _ = env.step(action)
+            state, reward, terminated, truncated, _ = env.step(action)
 
             gradients.append(gradient)
             rewards.append(reward)
+            score += reward
 
-            state = next_state
-            done = terminated or truncated
-
-        score = sum(rewards)
-        scores.append(score)
-
-        for i, gradient in enumerate(gradients):
+        for i in range(len(gradients)):
             discounted_reward = 0
 
-            for j, reward in enumerate(rewards[i:]):
-                discounted_reward += reward * (gamma ** j)
+            for j in range(i, len(rewards)):
+                discounted_reward += (
+                    rewards[j] * (gamma ** (j - i))
+                )
 
-            weight += alpha * gradient * discounted_reward
+            weight += alpha * gradients[i] * discounted_reward
 
+        scores.append(score)
         print("Episode: {} Score: {}".format(episode, score))
 
     return scores
